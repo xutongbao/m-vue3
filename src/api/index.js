efa668a03f731b3a50059429e9099c165ed7e99f
徐同保 <xutongbao@vackbot.com>
2019-07-10 14:50:13 +0800

    增加删除

--- src/api/index.js
+++ src/api/index.js
@@ -5,5 +5,6 @@ export default {
     //路由
     login: (data) => common({url: urls.login, method: 'post', data}),
     getList: (url) => common({url: urls.getList + url, method: 'get'}),
-    deleteItem: (data) => common({url: urls.deleteItem, method: 'post', data})
+    deleteItem: (data) => common({url: urls.deleteItem, method: 'post', data}),
+    addItem:(data) => common({url: urls.addItem, method: 'post', data})
 }
--- src/components/Controle.vue
+++ src/components/Controle.vue
@@ -4,10 +4,16 @@
         <button @click="handleFilterType(1)">双休日加班</button>
 		<button @click="handleFilterType(2)">节假日加班</button>
 		<button @click="handleFilterType(3)">工作日加班</button>
+		<button @click="handleAdd()">添加</button>
 	</div>
 </template>
 
 <script type="text/javascript">
+import Api from '@/api/index.js'
+
+function getID(length) {
+    return Number(Math.random().toString().substr(3, length) + Date.now()).toString(36);
+}
 
 export default {
 	name: 'Control',
@@ -17,7 +23,16 @@ export default {
         },
         handleFilterType(type) {
             this.$store.commit('filter', type)
-        }
+		},
+		handleAdd() {
+			let data = {
+				applicationNumber: getID(6)
+			}
+			Api.addItem(data).then((res) => {
+				console.log('添加')
+				this.$store.dispatch("initList");
+			})
+		}
 	}
 }
 </script>
