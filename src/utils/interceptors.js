ca43ac950f3f448c4bfc770b56e11659cfd4fee4
徐同保 <xutongbao@vackbot.com>
2019-07-11 17:42:36 +0800

    拦截器

--- src/utils/interceptors.js
+++ src/utils/interceptors.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 axios.interceptors.request.use(
     (config) => {
-        config.headers['authorization'] = localStorage.getItem('token');
-        return Promise.resolve(config)
+        config.headers['token'] = localStorage.getItem('token')
+        return config
     },
     (error) => {
         return Promise.reject(error.response);
